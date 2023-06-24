b2110d8f866ce1d480dc034be907d88ae556da3e
Flynn Duniho <flynn@harveyosity.org>
2023-06-23 22:08:36 -0700

    Start adding capabilities for the computer to guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,51 @@
-use std::{io, num};
+use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main() {
-    println!("Guess the number!");
+enum GameType {
+   PlayerGuess,
+   ComputerGuess,
+}
 
+fn main() {
+    let mut game_response = String::new();
+
+    println!("Should the player guess the number? (y/n)");
+
+    io::stdin()
+            .read_line(&mut game_response)
+            .expect("Failed to read line");
+
+    let is_player_guess = game_response.trim().to_lowercase() == "y";
+    let game_type; 
+
+    if is_player_guess {
+        game_type = GameType::PlayerGuess;
+    } 
+    else {
+        game_type = GameType::ComputerGuess;
+    }
+
+    let guess_count;
+
+    match game_type {
+        GameType::ComputerGuess => guess_count = computer_guess(),
+        GameType::PlayerGuess => guess_count = player_guess(),
+    }
+
+    println!("Took {guess_count} guesses!");
+}
+
+fn computer_guess() -> u32 {
+    // Implement computer guessing
+    println!("The computer's guessing!");
+    return 0;
+}
+
+fn player_guess() -> u32 {
+    let mut guess_count = 0;
+    println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    // println!("The secret number is {secret_number}");
 
     loop {
         println!("Enter your guess.");
@@ -24,8 +63,10 @@ fn main() {
                 continue;
             }
         };
-        REMOVE BUILD ARTIFACTS FROM GIT REPO
+
         println!("Your guess was {guess}");
+
+        guess_count += 1;
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -36,4 +77,6 @@ fn main() {
                 },
         }
     }
+
+    return guess_count;
 }
