aa2c4848b815a6bd5d4033624ecd564cd6a97e26
Flynn Duniho <flynn@harveyosity.org>
2023-06-23 23:15:45 -0700

    guessing game can now guess your number as well as let the player guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -26,26 +26,68 @@ fn main() {
         game_type = GameType::ComputerGuess;
     }
 
-    let guess_count;
+    let guess_count: u32;
+
+    let min = 1;
+    let max = 100;
 
     match game_type {
-        GameType::ComputerGuess => guess_count = computer_guess(),
-        GameType::PlayerGuess => guess_count = player_guess(),
+        GameType::ComputerGuess => guess_count = computer_guess(min, max),
+        GameType::PlayerGuess => guess_count = player_guess(min, max),
     }
 
     println!("Took {guess_count} guesses!");
 }
 
-fn computer_guess() -> u32 {
-    // Implement computer guessing
-    println!("The computer's guessing!");
-    return 0;
+fn computer_guess(guess_min: i32, guess_max: i32) -> u32 {
+    let mut guess_count: u32 = 0;
+    let mut min = guess_min;
+    let mut max = guess_max;
+
+    println!("Pick a number from {min} to {max}");
+
+    loop {
+        let guess = (min + max) / 2;
+
+        println!("Is your number {guess}? (h/l/c)");
+
+        let mut response = String::new();
+
+        io::stdin()
+            .read_line(&mut response)
+            .expect("Failed to read line");
+
+        let response = response.trim().to_lowercase();
+
+        guess_count += 1;
+
+        match response.as_str() {
+            "h" => min = guess + 1,
+            "l" => max = guess - 1,
+            "c" => break,
+            _ => {
+                println!("Invalid response!");
+                guess_count -= 1;
+                continue;
+            }
+        }
+
+        if min > max {
+            println!("You're cheating!");
+            min = guess_min;
+            max = guess_max;
+            guess_count = 0;
+        }
+    }
+
+    return guess_count;
 }
 
-fn player_guess() -> u32 {
-    let mut guess_count = 0;
+fn player_guess(min: i32, max: i32) -> u32 {
+    let mut guess_count: u32 = 0;
+    let secret_number = rand::thread_rng().gen_range(min..=max);
+
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
         println!("Enter your guess.");
@@ -56,7 +98,7 @@ fn player_guess() -> u32 {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Not a number!");
@@ -69,8 +111,8 @@ fn player_guess() -> u32 {
         guess_count += 1;
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Higher!"),
+            Ordering::Greater => println!("Lower!"),
             Ordering::Equal => {
                     println!("Correct!");
                     break;
