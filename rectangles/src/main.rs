1e6cb947ae0556ca1db0c0e3dc7702aa72e196ed
Flynn Duniho <flynn@harveyosity.org>
2023-07-14 22:36:52 -0700

    finish rectangles project

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,16 @@ impl Rectangle {
     fn area (&self) -> u32 {
         self.width * self.height
     }
+    
+    fn can_hold (&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl std::fmt::Display for Rectangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}x{}", self.width, self.height)
+    }
 }
 
 fn main() {
@@ -15,10 +25,22 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 65,
+    };
 
-    println!("rect1 is {:?}", rect1);
+    // println! is a special case and converts to borrowed references automatically
     println!(
-        "The area of the rectangle is {} square units",
-        rect1.area()
+        "The area of a {} rectangle is {} square units",
+        rect1, rect1.area()
         );
+
+    if rect3.can_hold(&rect1) {
+        println!("A rectangle of dimensions {} can hold a rectangle of dimensions {}", rect3, rect1);
+    }
 }
